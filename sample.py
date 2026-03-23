5197c6c1db4a9284f3acb1252c8fc53b4f8a5554
DracoRooks <gokulmittal008@gmail.com>
2026-03-24 03:27:17 +0530

    add: batch normalisation optimisation.

--- sample.py
+++ sample.py
@@ -3,23 +3,36 @@ import torch
 from torch.nn import functional as func
 
 # Loading The Pre-trained Octagram Language Model
-modelToSample = "./datasets/octagram_param_configuration.pth"
-state = torch.load("./datasets/octagram_param_configuration.pth", weights_only = False)
+modelToSample = "./datasets/32gram_param_configuration_optimized.pth"
+state = torch.load(modelToSample, weights_only = False)
 params = state["params"]
 
 # Loading params from the saved state
 uniqueChars = params["uniqueChars"]
 hyperParams = params["hyperParams"]
+batchNormParams = params["batchNormParams"]
 freeParams = params["freeParams"]
 
 # Reading all freeParams from the saved file
 embed = freeParams["embedding"]
 
 w1 = freeParams["w1"]
-b1 = freeParams["b1"]
+bngain1 = freeParams["bngain1"]
+bnbias1 = freeParams["bnbias1"]
 
 w2 = freeParams["w2"]
-b2 = freeParams["b2"]
+bngain2 = freeParams["bngain2"]
+bnbias2 = freeParams["bnbias2"]
+
+w3 = freeParams["w3"]
+b3 = freeParams["b3"]
+
+# Reading Batch Normalisation Parameters
+bnmean1_running = batchNormParams["bnmean1_running"]
+bnstd1_running = batchNormParams["bnstd1_running"]
+
+bnmean2_running = batchNormParams["bnmean2_running"]
+bnstd2_running = batchNormParams["bnstd2_running"]
 
 # Reading the rng from hyperParams
 gen = hyperParams["gen"]
@@ -37,12 +50,21 @@ decode = lambda idx: itos[idx]
 result = []
 for _ in range(20):
     line = ""
-    context = [0] * 7
+    context = [0] * hyperParams["blockSize"]
     while True:
         x = embed[torch.tensor(context)].view(1, -1)
 
-        h = ((x @ w1) + b1).tanh()
-        logits = (h @ w2) + b2
+        h1preact = x @ w1
+        h1preact = (h1preact - bnmean1_running) / bnstd1_running
+        h1preact = bngain1 * h1preact + bnbias1
+        h1 = h1preact.tanh()
+
+        h2preact = h1 @ w2
+        h2preact = (h2preact - bnmean2_running) / bnstd2_running
+        h2preact = bngain2 * h2preact + bnbias2
+        h2 = h2preact.tanh()
+
+        logits = (h2 @ w3) + b3
         probs = func.softmax(logits, dim = 1)
 
         y = torch.multinomial(probs, 1, True, generator = gen).item()
